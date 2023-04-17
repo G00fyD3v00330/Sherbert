32e7076f45c1477aa97ceadb86ffb9dfdf4fbbc1
G00fyD3v00330 <130918202+G00fyD3v00330@users.noreply.github.com>
2023-04-17 09:35:33 +0000

    bug fixes and things

--- main.py
+++ main.py
@@ -18,44 +18,24 @@ t = Translator()
 
 
 def generateUwU(input_text):
-
-  # the length of the input text
   length = len(input_text)
-
-  # variable declaration for the output text
   output_text = ''
-
-  # check the cases for every individual character
   for i in range(length):
-
-    # initialize the variables
     current_char = input_text[i]
     previous_char = '&# 092;&# 048;'
-
-    # assign the value of previous_char
     if i > 0:
       previous_char = input_text[i - 1]
-
-    # change 'L' and 'R' to 'W'
     if current_char == 'L' or current_char == 'R':
       output_text += 'W'
-
-    # change 'l' and 'r' to 'w'
     elif current_char == 'l' or current_char == 'r':
       output_text += 'w'
-
-    # if the current character is 'o' or 'O'
-    # also check the previous character
     elif current_char == 'O' or current_char == 'o':
       if previous_char == 'N' or previous_char == 'n' or previous_char == 'M' or previous_char == 'm':
         output_text += "yo"
       else:
         output_text += current_char
-
-    # if no case match, write it as it is
     else:
       output_text += current_char
-
   return output_text
 
 
@@ -170,7 +150,7 @@ async def hack(ctx, user: discord.Member = None, localIP = "153.23.34.321", loca
     await asyncio.sleep(1)
     await msg.edit("Scanning for strange files... ``(Search_Settings: SearchingFor: {kids, kids, kids})``")
     await asyncio.sleep(1)
-    await msg.edit("194,503 strange files found. Reporting to FBI")
+    await msg.edit("194,503 strange files found. Reporting  FBI")
     await asyncio.sleep(1)
     if localhacksettings == "-sherbert":
       await asyncio.sleep(1)
@@ -184,5 +164,5 @@ async def hack(ctx, user: discord.Member = None, localIP = "153.23.34.321", loca
     await asyncio.sleep(1)
     await msg.edit(f"{user} has been hacked successfully!")
 
-token = os.environ.get("TOKEN") 
+token = os.environ.get("DISCORD_TOKEN") 
 client.run(token)
